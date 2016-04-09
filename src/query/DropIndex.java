c57bb4e7f7a6ec6b66022a97840a7d3b6c8841af
yin56 <yin56@purdue.edu>
2016-04-09 14:02:16 -0400

    Update DropIndex.java

--- src/query/DropIndex.java
+++ src/query/DropIndex.java
@@ -1,11 +1,17 @@
 package query;
 
+import index.HashIndex;
+import global.Minibase;
 import parser.AST_DropIndex;
 
 /**
  * Execution plan for dropping indexes.
  */
 class DropIndex implements Plan {
+	
+	//AST_DropIndex tree;
+	//AST_DropTable table;
+	private String fileName;
 
   /**
    * Optimizes the plan, given the parsed query.
@@ -13,16 +19,21 @@ class DropIndex implements Plan {
    * @throws QueryException if index doesn't exist
    */
   public DropIndex(AST_DropIndex tree) throws QueryException {
-
-  } // public DropIndex(AST_DropIndex tree) throws QueryException
+	//this.tree = tree;
+	//tree.jjtOpen();
+	  fileName = tree.getFileName();
+	  QueryCheck.indexExists(fileName);
+	//public DropIndex(AST_DropIndex tree) throws QueryException
+  }  
 
   /**
    * Executes the plan and prints applicable output.
    */
   public void execute() {
-
-    // print the output message
-    System.out.println("(Not implemented)");
+    // print the output message;
+	  new HashIndex(fileName).deleteFile();
+	  Minibase.SystemCatalog.dropIndex(fileName);
+	  System.out.println("Index dropped.");
 
   } // public void execute()
 
