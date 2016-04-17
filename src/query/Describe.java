29b1fe4779226e836ca0136487b597d77a371ea1
Sean Michael Kelley <kelley60@purdue.edu>
2016-04-16 20:33:18 -0400

    Describe.java

--- src/query/Describe.java
+++ src/query/Describe.java
@@ -1,28 +1,49 @@
 package query;
 
+import global.AttrType;
+import heap.HeapFile;
 import parser.AST_Describe;
+import relop.Schema;
 
 /**
  * Execution plan for describing tables.
  */
 class Describe implements Plan {
 
+	//fileName of the table
+	private String tableName;
+	
+	private Schema schema;
+	
+	private HeapFile heapFile;
+
+	
   /**
    * Optimizes the plan, given the parsed query.
    * 
    * @throws QueryException if table doesn't exist
    */
   public Describe(AST_Describe tree) throws QueryException {
-
+	  
+	  tableName = tree.getFileName();
+	  schema = QueryCheck.tableExists(tableName);
+	  //heapfile that has table that needs to have values inserted
+	  heapFile = new HeapFile(tableName);
+	  
   } // public Describe(AST_Describe tree) throws QueryException
 
   /**
    * Executes the plan and prints applicable output.
    */
   public void execute() {
-
-    // print the output message
-    System.out.println("(Not implemented)");
+	  
+	  int length = schema.getCount();
+	  for (int i = 0; i < length; i++){
+		  String columnName = schema.fieldName(i);
+		  String columnType = AttrType.toString(schema.fieldType(i));
+		  System.out.println("Column name is " + columnName + " and column type is " + columnType);
+	  }
+	  
 
   } // public void execute()
 
